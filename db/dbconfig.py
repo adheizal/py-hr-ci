11e4aefb6a6d314c3e24c454cf0e27416101b429
adhe lhgi <fachrizalfahmy@gmail.com>
2018-10-04 13:44:07 +0000

    Update dbconfig.py

--- db/dbconfig.py
+++ db/dbconfig.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 #Database setting
 
 MONGO_URI = os.environ.get('MONGOHQ_URL') if os.environ.get('MONGOHQ_URL') is not None \
-    else 'mongodb://adhelhgi:Cintamatiku20`@ds223343.mlab.com:23343/testlhgi'
+    else 'mongodb://heroku_tr2f0kzz:6vgb9kukuk3tg1berdjegh3vn4@ds033175.mlab.com:33175/heroku_tr2f0kzz'
 
 client = MongoClient(MONGO_URI)
 DB = client.get_default_database().name if client.get_default_database() is not None else 'task_database'
